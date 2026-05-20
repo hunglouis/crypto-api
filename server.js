792fa2ec9f9d8398c63c473d3c800810ef3637c0
hunglouis <83754071+hunglouis@users.noreply.github.com>
2026-05-20 14:30:40 +0700

    fix render

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const cors = require('cors');
-const PORT =
-  process.env.PORT || 3002;
 
 const app = express();
 
-app.use(cors({
-  origin: '*'
-}));
+app.use(cors());
 
-const ratesRoute =
-  require('./routes/rates');
+app.get('/', (req, res) => {
 
-app.use('/api/rates', ratesRoute);
+  res.send('Backend alive');
 
+});
 
-server.listen(PORT, () => {
+app.get('/api/rates', (req, res) => {
+
+  res.json({
+    rates: {
+      MATIC: 0.74,
+      VND: 26000
+    }
+  });
+
+});
+
+const PORT =
+  process.env.PORT || 3002;
+
+app.listen(PORT, () => {
 
   console.log(
     `Server running on ${PORT}`
