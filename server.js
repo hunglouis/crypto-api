82648d7d5372d246d9c5aef408d66054b5466466
hunglouis <83754071+hunglouis@users.noreply.github.com>
2026-05-21 11:04:56 +0700

    cap nhat 11:05 21/05/2026

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 
@@ -36,5 +37,25 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
 
   res.send('Crypto API running');
+
+});
+
+app.get('/api/rates/eth', async (req, res) => {
+
+  try {
+
+    const response = await axios.get(
+      'https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd,vnd'
+    );
+
+    res.json(response.data);
+
+  } catch (err) {
+
+    res.status(500).json({
+      error: 'Không lấy được giá'
+    });
+
+  }
 
 });
